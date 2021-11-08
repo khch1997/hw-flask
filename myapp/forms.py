a02c0dba88d4523322d30db4ae6bcd569af2c223
Khanh Chung <justinngo1997@gmail.com>
2021-11-08 13:30:21 -0800

    Capitalize visited label

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired, required
 class TopCities(FlaskForm):
   city_name = StringField("City Name", validators=[DataRequired()])
   city_rank = IntegerField("City Rank", [validators.required()])
-  is_visited = BooleanField("visited")
+  is_visited = BooleanField("Visited")
   submit = SubmitField("Submit")
